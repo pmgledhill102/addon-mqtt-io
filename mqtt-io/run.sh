80b7c7bf30db6e26547edf2499473fd11966fd8a
Paul Gledhill <github@pmgledhill.com>
2022-07-26 12:26:55 +0100

    Initial Commit

--- mqtt-io/run.sh
+++ mqtt-io/run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
 echo "Hello world!"
+
+MQTT_HOST=$(bashio::services mqtt "host")
+MQTT_USER=$(bashio::services mqtt "username")
+MQTT_PASSWORD=$(bashio::services mqtt "password")
+
+python3 -m mqtt_io --render render.yaml mqtt-config.yml
